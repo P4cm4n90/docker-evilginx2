b134c66c7ec5daf888ab3865d5244a355f1a36a8
Damian <50337469+P4cm4n90@users.noreply.github.com>
2024-07-19 10:19:04 +0200

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-exec mkdir -p /root/.evilginx/
-exec cp /app/blacklist.txt /root/.evilginx/blacklist.txt
+mkdir -p /root/.evilginx/
+cp /app/blacklist.txt /root/.evilginx/blacklist.txt
 echo "Starting Evilginx2..."
 exec /bin/evilginx -p /app/phishlets -debug
